568e78bdc7ce168e0ad5d4b9cdacd8e60ba26af6
Pradeep <iampradeephr@gmail.com>
2023-03-28 22:02:56 +0530

    Initial Commit

--- src/Arrays/KadanesAlgorithm.java
+++ src/Arrays/KadanesAlgorithm.java
@@ -1,5 +1,6 @@
 package Arrays;
 
+
 public class KadanesAlgorithm {
 
     //Goal - To find the sum of max sub array
@@ -21,10 +22,21 @@ public class KadanesAlgorithm {
         return maxSum;
     }
 
+    public static int maxSubArray2(int[] arr){
+        int max_so_far = arr[0];
+        int max_ending_here = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            max_ending_here = Math.max(arr[i], max_ending_here + arr[i]);
+            max_so_far = Math.max(max_so_far, max_ending_here);
+        }
+        return max_so_far;
+    }
+
     public static void main(String[] args) {
 
-        int arr[] ={5,-4,-2,6,-1};
-        int ans=maxSubArray(arr);
+        int arr1[] ={5,-4,-2,6,-1};
+        int arr2[]={-3, -7, -2, -8, -9};
+        int ans=maxSubArray2(arr1);
         System.out.println("Ans: "+ans);
 
     }
